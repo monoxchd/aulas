a700e57fc847fde7ff8d096395459c3ba36f16d3
monoxchd <dneves.junior@gmail.com>
2013-05-27 20:39:42 -0300

    first commit

---
+++ src/dominio/Disciplina.java
@@ -0,0 +1,25 @@
+package dominio;
+
+public class Disciplina 
+{
+	public int horario, sala;
+	public boolean disponibilidade;
+	Aluno[] alunos;
+	
+	public void setHorario(int horario)
+	{
+		this.horario = horario;
+	}
+	
+	public void setSala(int sala)
+	{
+		this.sala = sala;
+	}
+	
+	public
+	
+	public void setDisponibilidade(boolean disponibilidade)
+	{
+		this.disponibilidade = disponibilidade;
+	}
+}
